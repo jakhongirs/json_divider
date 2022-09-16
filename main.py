7b91239962ab72d17066468fae6bf131ae3aaa88
jakhongirs <j.sagdullaev31@gmail.com>
2022-09-16 11:00:21 +0500

    fix: initial

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ def split(a, n):
     return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
 
 
-with open('modification_type _uz.json') as data_file:
+with open('file.json') as data_file:
     data = json.load(data_file)
 
     sorted_data = list(split(data, 3))
 
-    with open('modification_type_uz_1.json', 'w', encoding='utf-8') as f:
-        json.dump(sorted_data[1], f, ensure_ascii=False, indent=4)
+    with open('new_file.json', 'w', encoding='utf-8') as f:
+        json.dump(sorted_data[0], f, ensure_ascii=False, indent=4)
